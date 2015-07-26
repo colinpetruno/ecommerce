0e25d4b26149b52f1a24ea53d7a9fc4f4602ea3e
Colin Petruno <colinpetruno@gmail.com>
2015-07-25 22:15:21 -0400

    Fill Out Cart Page
    Add some methods to calcuate the totals for an order item and an order.
    Also add the display on the orders edit page for now.

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,8 @@ class Order < ActiveRecord::Base
 
   has_many :order_items, dependent: :destroy
   has_many :skus, through: :order_items
+
+  def total
+    order_items.map(&:total_price).inject(0, :+)
+  end
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,4 +2,12 @@ class OrderItem < ActiveRecord::Base
   belongs_to :sku
   belongs_to :order
   has_one :product, through: :sku
+
+  def full_name
+    "#{product.name} - #{sku.name}"
+  end
+
+  def total_price
+    sku.price * quantity
+  end
 end
