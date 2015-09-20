3b932a6cf4c706961ca9d85056cc909aa9d750a6
Colin Petruno <colinpetruno@gmail.com>
2015-09-20 00:41:08 -0400

    Update setting from DEPRECATION WARNING

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module Ecommerce
     ]
 
     # let heroku serve assets
-    config.serve_static_assets = true
+    config.serve_static_files = true
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
